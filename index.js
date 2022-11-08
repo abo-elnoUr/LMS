5b91c93abf96c7f8eada7f158774196553461ad2
abo-elnoUr <mohamed2030aboelnour@gmail.com>
2022-11-08 17:57:02 +0200

    update side content

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 function openPayment() {
     document.getElementById("payment").style.width = "500px";
     document.getElementById("payment").style.opacity = "1";
+    document.getElementById("main-section").style.marginRight = "500px";
   }
   
   function closePayment() {
     document.getElementById("payment").style.width = "0";
     document.getElementById("payment").style.opacity = "0";
+    document.getElementById("main-section").style.marginRight = "0px";
   }
 
