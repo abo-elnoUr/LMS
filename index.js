c59b96fb98598ed54ac04de514d59787cf59ac17
abo-elnoUr <mohamed2030aboelnour@gmail.com>
2022-11-08 16:38:14 +0200

    add side

---
+++ index.js
@@ -0,0 +1,10 @@
+function openPayment() {
+    document.getElementById("payment").style.width = "500px";
+    document.getElementById("payment").style.opacity = "1";
+  }
+  
+  function closePayment() {
+    document.getElementById("payment").style.width = "0";
+    document.getElementById("payment").style.opacity = "0";
+  }
+
